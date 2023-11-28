2886017f9e1f3e207300dcc48f0f56bc67446bcb
etherbits <nika.qvrivishviliPC@gmail.com>
2023-11-28 23:39:18 +0400

    adding charcoal colors to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        charcoal: {
+          "50": "#f6f7f9",
+          "100": "#eceef2",
+          "200": "#d5d9e2",
+          "300": "#b1bac8",
+          "400": "#8694aa",
+          "500": "#677790",
+          "600": "#525f77",
+          "700": "#434d61",
+          "800": "#3a4252",
+          "900": "#343a46",
+          "950": "#23272e",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
