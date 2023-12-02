9a83bae459c6ee3b9ab675798992698833df56c8
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-02 17:29:07 +0400

    implementing route guarding in middleware

---
+++ middleware.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "@/lib/lucia";
+import { env } from "@/app/env";
+import { Session } from "lucia";
+
+const publicPaths = ["/"];
+const authenticationPaths = ["/log-in", "/register"];
+
+function isPublic(request: NextRequest) {
+  const parsedUrl = new URL(request.url);
+  return publicPaths.includes(parsedUrl.pathname);
+}
+
+function shouldAuthenticate(request: NextRequest, session: Session | null) {
+  const parsedUrl = new URL(request.url);
+  return !session && !authenticationPaths.includes(parsedUrl.pathname);
+}
+
+function isAlreadyAuthenticated(request: NextRequest, session: Session | null) {
+  const parsedUrl = new URL(request.url);
+  return session && authenticationPaths.includes(parsedUrl.pathname);
+}
+
+export async function middleware(request: NextRequest) {
+  if (isPublic(request)) {
+    return NextResponse.next();
+  }
+
+  const authRequest = auth.handleRequest(request);
+  const session = await authRequest.validate();
+
+  if (shouldAuthenticate(request, session)) {
+    return new NextResponse("Please log in to access the page", {
+      status: 303,
+      headers: {
+        Location: new URL(env.AUTH_GUARD_PATH, request.url).href,
+      },
+    });
+  }
+
+  if (isAlreadyAuthenticated(request, session)) {
+    return new NextResponse("User already authenticated", {
+      status: 303,
+      headers: {
+        Location: new URL(env.DEFAULT_PATH, request.url).href,
+      },
+    });
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+};
--- utils/auth.ts
+++ utils/auth.ts
@@ -6,3 +6,4 @@ export const getPageSession = cache(() => {
   const authRequest = auth.handleRequest("GET", context);
   return authRequest.validate();
 });
+
