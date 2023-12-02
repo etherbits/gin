00edae677f1242cc317d6f9e06393e8d0297b092
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-02 15:16:15 +0400

    checking new envs

--- app/env.ts
+++ app/env.ts
@@ -4,4 +4,6 @@ import { z } from "zod";
 export const env = createServerEnv({
   DATABASE_URL: z.string(),
   NODE_ENV: z.enum(["production", "development", "test"]),
+  DEFAULT_PATH: z.string(),
+  AUTH_GUARD_PATH: z.string(),
 });
