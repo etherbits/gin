43964420ddf35baef77432ceba12e827e24906a1
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-05 15:41:30 +0400

    card deletion

---
+++ app/api/card/delete/route.ts
@@ -0,0 +1,13 @@
+import { db } from "@/db/drizzle";
+import { card } from "@/db/schema/deck";
+import { eq, sql } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const params = request.nextUrl.searchParams;
+  const id = params.get("id");
+
+  await db.delete(card).where(eq(card.id, sql`(UUID_TO_BIN(${id}))`));
+
+  return new Response("ok", { status: 200 });
+}
