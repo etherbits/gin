51d47fc57dd2648832b03c418632e31e4b560514
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-07 03:22:21 +0400

    getting only user decks and cards

--- app/api/card/get-all/route.ts
+++ app/api/card/get-all/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/db/drizzle";
 import { card } from "@/db/schema/deck";
 import { getRouteSession } from "@/utils/auth";
-import { sql } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import { getTableColumns } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
@@ -18,7 +18,8 @@ export async function GET(request: NextRequest) {
       id: sql`BIN_TO_UUID(${card.id})`,
       deckId: sql`BIN_TO_UUID(${card.deckId})`,
     })
-    .from(card);
+    .from(card)
+    .where(eq(card.userId, session.user.userId));
 
   return new Response(JSON.stringify(cards), { status: 200 });
 }
--- app/api/card/get/route.ts
+++ app/api/card/get/route.ts
@@ -1,29 +1,20 @@
-import { env } from "@/app/env";
 import { db } from "@/db/drizzle";
 import { card } from "@/db/schema/deck";
+import { getRouteSession } from "@/utils/auth";
 import { getResult, respondWithError } from "@/utils/errorHandling";
-import { eq, sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { getTableColumns } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const session = await getRouteSession(request);
+
+  if (!session) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   const params = request.nextUrl.searchParams;
   const id = params.get("id");
-
-  try {
-    console.log(
-      await db
-        .select({
-          ...getTableColumns(card),
-          id: sql`BIN_TO_UUID(${card.id})`,
-          deckId: sql`BIN_TO_UUID(${card.deckId})`,
-        })
-        .from(card)
-        .where(eq(card.id, sql`(UUID_TO_BIN(${id}))`)),
-    );
-  } catch (e) {
-    console.error(e);
-  }
 
   const [cards, error] = await getResult(async () => {
     return await db
@@ -33,7 +24,12 @@ export async function GET(request: NextRequest) {
         deckId: sql`BIN_TO_UUID(${card.deckId})`,
       })
       .from(card)
-      .where(eq(card.id, sql`(UUID_TO_BIN(${id}))`));
+      .where(
+        and(
+          eq(card.id, sql`(UUID_TO_BIN(${id}))`),
+          eq(card.userId, session.user.userId),
+        ),
+      );
   });
 
   if (error) {
