ce8e44d9277f3f811d1c0982312529ebb4b99c2f
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-07 01:00:02 +0400

    tracking db bug down #3.7

--- app/api/test/get/route.ts
+++ app/api/test/get/route.ts
@@ -1,8 +1,11 @@
 import { db } from "@/db/drizzle";
+import { user } from "@/db/schema/user";
 import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
+  await db.select().from(user);
+
   return new Response(request.method + " " + request.nextUrl.pathname + " ");
 }
