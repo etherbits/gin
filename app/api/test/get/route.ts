d41c3d7432120a13a3522145ec77e78923830f40
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-07 00:57:40 +0400

    tracking db bug down #3.5

--- app/api/test/get/route.ts
+++ app/api/test/get/route.ts
@@ -4,10 +4,5 @@ import { NextRequest } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
-  const users = await db.query.user.findMany();
-  console.log(users);
-
-  return new Response(
-    request.method + " " + request.nextUrl.pathname + " " + users[0].username,
-  );
+  return new Response(request.method + " " + request.nextUrl.pathname + " ");
 }
