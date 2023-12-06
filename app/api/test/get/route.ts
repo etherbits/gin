dcf0aa5fdc2a6c6282ae50c2e5c57ab33c42467c
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-07 00:47:24 +0400

    tracking db bug down #3.1

--- app/api/test/get/route.ts
+++ app/api/test/get/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return request.method + request.nextUrl.pathname;
+  return new Response(request.method + request.nextUrl.pathname)
 }
