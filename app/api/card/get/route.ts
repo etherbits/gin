a33456e676dcb99c6abf7b2c96e39d99efcda51b
etherbits <nika.qvrivishviliPC@gmail.com>
2023-12-06 23:03:59 +0400

    more testing

--- app/api/card/get/route.ts
+++ app/api/card/get/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db/drizzle";
 import { card } from "@/db/schema/deck";
+import { getResult, respondWithError } from "@/utils/errorHandling";
 import { eq, sql } from "drizzle-orm";
 import { getTableColumns } from "drizzle-orm";
 import { NextRequest } from "next/server";
@@ -8,15 +9,20 @@ export async function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams;
   const id = params.get("id");
 
-  
-  const cards = await db
-    .select({
-      ...getTableColumns(card),
-      id: sql`BIN_TO_UUID(${card.id})`,
-      deckId: sql`BIN_TO_UUID(${card.deckId})`,
-    })
-    .from(card)
-    .where(eq(card.id, sql`(UUID_TO_BIN(${id}))`));
+  const [cards, error] = await getResult(async () => {
+    return await db
+      .select({
+        ...getTableColumns(card),
+        id: sql`BIN_TO_UUID(${card.id})`,
+        deckId: sql`BIN_TO_UUID(${card.deckId})`,
+      })
+      .from(card)
+      .where(eq(card.id, sql`(UUID_TO_BIN(${id}))`));
+  });
+
+  if (error) {
+    return respondWithError({ error, status: 400 });
+  }
 
   return new Response(JSON.stringify(cards), { status: 200 });
 }
