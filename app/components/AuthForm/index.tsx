b6d81dcfeb6ad56edfc3201a6e347d7574469885
etherbits <nika.qvrivishviliPC@gmail.com>
2024-01-14 08:00:00 +0000

    adding auth form

---
+++ app/components/AuthForm/index.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { LoginData,  loginSchema } from "@/validation-schemas/auth"
+import Form from "../Form"
+import Input from "../Input"
+import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
+import { useState } from "react"
+import ErrorLine from "../ErrorLine"
+
+export default function AuthForm() {
+  const router = useRouter()
+
+  const [error, setError] = useState<string | null>(null)
+
+  async function register(loginData: LoginData) {
+    return new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/log-in", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(loginData),
+      })
+
+      const data = await response.json()
+
+      if (response.ok) {
+        router.replace("/home")
+        return resolve(data)
+      }
+      
+      setError(data.message)
+      reject(data)
+    })
+  }
+
+  return (
+    <Form
+      title="Log in"
+      description="Log in to your account."
+      schema={loginSchema}
+      onSubmit={async (values) => {
+        setError(null)
+
+        toast.promise(register(values), {
+          pending: "Logging in...",
+          success: "Authenticated!",
+          error: "Authentication failed",
+        })
+      }}
+      fields={(formProps) => (
+        <>
+          <Input
+            name="email"
+            label="Username"
+            type="text"
+            placeholder="BigGuy"
+            {...formProps}
+          />
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+            placeholder="••••••••"
+            {...formProps}
+          />
+          <button type="submit">Log In</button>
+          {error && <ErrorLine severity="error" message={error}/>}
+        </>
+      )}
+    />
+  )
+}
--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,43 +1,9 @@
-"use client"
+import AuthForm from "../components/AuthForm";
 
 export default async function LogIn() {
   return (
     <main>
-      <form
-        className="flex flex-col gap-4 max-w-[600px] mx-auto m-16"
-        action="/api/log-in"
-        method="POST"
-        onSubmit={async (e) => {
-          e.preventDefault()
-
-          const data = {
-            email: "asd@asd.com",
-            password: "asdasdasd",
-          }
-
-          const res = await fetch("/api/log-in", {
-            method: "POST",
-            body: JSON.stringify(data),
-          })
-
-          console.log(res.ok)
-        }}
-      >
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <button>Log In</button>
-        <a href="/api/log-in/github" className="bg-slate-700 p-2">
-          Log in with github
-        </a>
-        <a href="/api/log-in/google" className="bg-red-700 p-2">
-          Log in with google
-        </a>
-        <a href="/api/log-in/discord" className="bg-blue-700 p-2">
-          Log in with discord
-        </a>
-      </form>
+      <AuthForm />
     </main>
   )
 }
