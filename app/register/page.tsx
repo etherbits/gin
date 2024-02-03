61932e51df120b3caa476dd1a74e8239a7bf9342
etherbits <nika.qvrivishviliPC@gmail.com>
2024-02-03 16:23:00 +0400

    chaning format script

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,7 +6,7 @@ export default async function Registration() {
     <main className="flex justify-center">
       <section className="mt-[3%] flex w-[500px] flex-col gap-4 rounded-md bg-charcoal-900">
         <RegistrationForm />
-        <section className="flex gap-4 px-8 py-6 justify-center">
+        <section className="flex justify-center gap-4 px-8 py-6">
           <OAuthProviders />
         </section>
       </section>
