db810cb1565a7e6d6df77fbf7c943d8fec56c612
etherbits <nika.qvrivishviliPC@gmail.com>
2024-02-06 13:11:35 +0400

    adding gin logo (FIX THE MIDDLEWARE WHAT ARE YOU SMOKING)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import Image from "next/image"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,15 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-charcoal-950 px-8 py-12 text-charcoal-200`}
       >
+        <header>
+          <Image
+            src="/vectors/gin-logo.svg"
+            alt="Gin Logo"
+            width={100}
+            height={100}
+          />
+          Gin
+        </header>
         {children}
         <ToastContainer theme="dark" position="bottom-right" />
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link"
 export default async function Landing() {
   return (
     <main>
-      Gink
       <Link href="/log-in"> Log In </Link>
       <Link href="/register"> Register </Link>
       <Link href="/home"> Home </Link>
