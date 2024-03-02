91bd66f90a2b53e8faac0ce87d2b1bf9e90384f4
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-03-02 23:41:19 +0400

    implementing sign in

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,17 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import { parsedEnv } from "@/utils/env";
+import * as userSchemas from "@/db/schemas/user";
+import * as deckSchemas from "@/db/schemas/deck";
+
+const schema = {
+  ...userSchemas,
+  ...deckSchemas,
+};
 
 const turso = createClient({
   url: parsedEnv.TURSO_DATABASE_URL,
   authToken: parsedEnv.TURSO_AUTH_TOKEN,
 });
 
-export const db = drizzle(turso);
+export const db = drizzle(turso, { schema });
