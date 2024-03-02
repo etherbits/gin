3fb25abc69beb1c9757b54fa2670aa35457cc1d9
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-03-02 23:31:17 +0400

    implementing sign up

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,58 @@
+import { db } from "@/db";
+import { user } from "@/db/schemas/user";
+import { lucia } from "@/lib/auth";
+import { generateId } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Argon2id } from "oslo/password";
+
+export default async function Page() {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <form action={signup}>
+        <label htmlFor="username">Username</label>
+        <input name="username" id="username" />
+        <br />
+        <label htmlFor="email">Email</label>
+        <input name="email" id="email" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <br />
+        <button>Continue</button>
+      </form>
+    </>
+  );
+}
+
+async function signup(formData: FormData) {
+  "use server";
+  const username = formData.get("username") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  if (!username || !email || !password) {
+    return;
+  }
+
+  const hashedPassword = await new Argon2id().hash(password);
+  const userId = generateId(15);
+
+  // TODO: check if username is already used
+  await db.insert(user).values({
+    id: userId,
+    username: username,
+    email: email,
+    hashed_password: hashedPassword,
+  });
+
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+  return redirect("/");
+}
