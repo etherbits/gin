1eeab2f648327585d020d18c3514a01ae0a1268a
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-03-04 17:37:12 +0400

    adding email verification with code func

---
+++ app/email-verification/page.tsx
@@ -0,0 +1,96 @@
+import { db } from "@/db";
+import { emailVerificationCodes, users } from "@/db/schemas/user";
+import { lucia } from "@/lib/auth";
+import { eq } from "drizzle-orm";
+import { User } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { isWithinExpirationDate } from "oslo";
+
+export default function Page() {
+  return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <form action={verifyEmail}>
+          <label htmlFor="code">Verification Code</label>
+          <input type="text" id="code" name="code" />
+          <button>Verify</button>
+        </form>
+      </main>
+    </div>
+  );
+}
+
+async function verifyEmail(formData: FormData) {
+  "use server";
+  const sessionId = cookies().get(lucia.sessionCookieName);
+  if (sessionId === undefined) {
+    return "No session cookie found";
+  }
+
+  const { user } = await lucia.validateSession(sessionId.value);
+  if (!user) {
+    return "Invalid session cookie";
+  }
+
+  const code = formData.get("code");
+  if (typeof code !== "string") {
+    return "Invalid code was provided";
+  }
+
+  const validCode = await verifyEmailCode(user, code);
+  if (!validCode) {
+    return "The provided code is invalid or expired";
+  }
+
+  await lucia.invalidateUserSessions(user.id);
+
+  await db
+    .update(users)
+    .set({ email_verified: 1 })
+    .where(eq(users.id, user.id));
+
+  const session = await lucia.createSession(user.id, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+
+  return redirect("/home");
+}
+
+async function verifyEmailCode(user: User, code: string) {
+  const dbCode = await db.transaction(async (tx) => {
+    const dbCode = await tx.query.emailVerificationCodes.findFirst({
+      where: (code, { eq }) => eq(code.userId, user.id),
+    });
+
+    if (!dbCode || dbCode.code !== code) {
+      return;
+    }
+
+    await tx
+      .delete(emailVerificationCodes)
+      .where(eq(emailVerificationCodes.id, dbCode.id));
+
+    return dbCode;
+  });
+
+  if (!dbCode) {
+    return false;
+  }
+
+  const codeExpirationDate = new Date(dbCode.expiresAt);
+
+  if (
+    !isWithinExpirationDate(codeExpirationDate) ||
+    dbCode.email !== user.email
+  ) {
+    return false;
+  }
+
+  return true;
+}
---
+++ db/migrations/0008_wakeful_justice.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `password_reset_tokens` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+/*
+ SQLite does not support "Creating foreign key on existing column" out of the box, we do not generate automatic migration for that, so it has to be done manually
+ Please refer to: https://www.techonthenet.com/sqlite/tables/alter_table.php
+                  https://www.sqlite.org/lang_altertable.html
+
+ Due to that we don't generate migration automatically and it has to be done manually
+*/
