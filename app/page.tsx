b052fb737218e8643c52c2e5ff8a6bbe8a39fe17
Nika Qvrivishvili <nika.qvrivishviliPC@gmail.com>
2024-03-13 21:29:41 +0400

    testing deploy on pr merge

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/sign-in">Sign In</Link>
+      <Link href="/sign-up">Sign Up</Link>
     </main>
   );
 }
